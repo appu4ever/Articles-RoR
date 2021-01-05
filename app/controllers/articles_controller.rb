78545b11495795fc9a90f60f50ce2370f0874523
Aparna Achuthan <appu4ever@gmail.com>
2021-01-05 20:07:05 +0530

    DRY and CRUD implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,52 @@
-class ArticlesController < ActionController::Base
+class ArticlesController < ApplicationController
+    before_action :find, only:[:show, :update, :destroy, :edit]
     def show
-        @article = Article.find(params[:id])
+        
     end
 
     def index
         @articles = Article.all
     end
     def new
-
+        @article = Article.new
     end
 
     def create
-
+        @article = Article.new(white_list_params)
+        if @article.save
+            flash[:notice] = "Added an article!!"
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
+    def edit
+        
+    end
+
+    def update    
+        @article.update(white_list_params)
+        if @article.save
+            flash[:notice] = "Updated an article!!"
+            redirect_to @article           
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        flash[:notice] = "deleted article #{params[:id]}!!"
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+    def find
+        @article = Article.find(params[:id])
+    end
+
+    def white_list_params
+        params.require(:article).permit(:title, :description)
+    end
 end
