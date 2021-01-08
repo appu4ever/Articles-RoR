5fe594cbb776a19482240615cd406cb40e3a7b82
Aparna Achuthan <appu4ever@gmail.com>
2021-01-08 20:15:28 +0530

    Added helper function to get gravatar url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+require 'digest/md5'
 module ApplicationHelper
+    def get_profile_url(user, options = {size: 80})
+        hash = Digest::MD5.hexdigest(user.email.downcase)
+        size = options[:size]
+        image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(image_src, alt: user.username, class: "rounded mx-auto d-block")
+    end
 end
