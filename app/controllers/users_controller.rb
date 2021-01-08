dc6ea13a5e3be2282ad3bd57be5bb2000db2144c
Aparna Achuthan <appu4ever@gmail.com>
2021-01-08 18:35:13 +0530

    Add edit controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :find, only: [:edit, :update]
+
     def new
         @user = User.new
     end
@@ -13,8 +15,27 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+
+    end
+
+    def update
+        @user.update(whitelist_params)
+        if @user.save
+            flash[:notice] = "User account updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
     private
+
     def whitelist_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find
+        @user = User.find(params[:id])
     end
 end
